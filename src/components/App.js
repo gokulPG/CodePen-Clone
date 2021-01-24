a8e6af4650377127cacadb73d1b111d9f8b12b5f
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2021-01-24 20:36:53 +0530

    editor setup done with codemirror text editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,25 @@
+import React from 'react'
+import Editor from './Editor'
 
 function App() {
-  return "Hello World"
+  return (
+    <>
+      <div className='pane top-game'>
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      <div className='pane'>
+        <iframe 
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder='0'
+          width='100%'
+          height='100%'
+        />
+      </div>
+    </>
+  );
 }
 
-export default App;
+export default App
