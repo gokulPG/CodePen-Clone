90f47a3a653dea63e4d122ece5c998188aa4bcf0
Gokul P Gangadharan <gokulgangadharanp@gmail.com>
2021-01-24 21:36:35 +0530

    cached snippets and resizable text editors

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react'
+
+const PREFIX = 'codepen-clone-'
+
+export default function useLocalStorage(key, initialValue) {
+
+    let prefixedKey = PREFIX + key
+
+    const [value, setValue] = useState(() => {
+        
+        const jsonValue = localStorage.getItem(prefixedKey)
+        if(jsonValue != null) return JSON.parse(jsonValue)
+
+        if(typeof initialValue === 'function') {
+            return initialValue()
+        } else {
+            return initialValue
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value])
+
+    return [value, setValue]
+}
